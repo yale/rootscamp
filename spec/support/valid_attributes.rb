f432e99df61c866c5f6389a819dc6c3584f2e722
Yale Spector <yspector@mdsol.com>
2012-02-16 14:47:11 -0500

    adding badge_id

--- spec/support/valid_attributes.rb
+++ spec/support/valid_attributes.rb
@@ -2,6 +2,7 @@ VALID_ATTRIBUTES = {
   :user => {
     :first_name   => "Christopher",
     :last_name    => "Hitchens",
+    :badge_id     => "314",
     :phone   => "1-555-867-5309",
     :email   => "chris@example.com",
     :twitter => "@chrishitchens",
